0b75e117a1c53fc2b4d94cc2779736d9da596b58
TRUONG Huu-Tien <truong.htien@gmail.com>
2017-05-27 15:18:40 -0400

    Add React

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+import path from 'path';
 import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import config from './src/config';
+import Html from './src/view/Html.js';
 
 const app = express();
+
+
+/* React */
+app.get('*', (req, res, next) => {
+  const html = ReactDOMServer.renderToStaticMarkup(<Html />);
+  res.send(`<!doctype html>${html}`);
+});
 
 app.listen(config.port, () => {
   console.info(`The server is running at http://localhost:${config.port}/`);
---
+++ src/view/Html.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Html extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+          <div id="root"></div>
+        </body>
+      </html>
+    );
+  }
+}
+
+export default Html;
