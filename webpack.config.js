4ffd5aea3ea7de3d17a54052b6975f4034b686b8
TRUONG Huu-Tien <truong.htien@gmail.com>
2017-05-27 14:26:59 -0400

    Add Babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
